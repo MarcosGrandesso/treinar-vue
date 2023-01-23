68232402f11388ea177df355191f569898f42c95
Marcos Grandesso <marcosgrandesso@hotmail.com>
2023-01-23 13:00:39 -0300

    busca user e repos

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,13 @@
 export const api = {
-  search_users(string) {
-    return fetch(`https://api.github.com/search/users?q=${string}`).then(res => res.json())
+  async search_users(string) {
+    const result = await fetch(`https://api.github.com/search/users?q=${string}`)
+    return await result.json()
+  },
+  async lista_repos(string, page= false) {
+    if (!page) {
+      page = 1
+    }
+    const result = await fetch(`https://api.github.com/users/${string}/repos?page=${page}`)
+    return await result.json()
   }
 }
--- src/components/GithubRepo.vue
+++ src/components/GithubRepo.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    <v-autocomplete
+    <v-row class="text-center">
+      <v-col cols="6">
+        <v-autocomplete
         v-model="user"
         :items="userlist"
         :loading="userloading"
         :search-input.sync="usersearch"
         item-text="login"
         />
+      </v-col>
+      <v-col cols="6">
+        <v-select
+          v-model="repo"
+          :items="repolist"
+          item-text="name"
+          :loading="repoloading"
+          label="Selecione o repositorio"
+          return-object
+          single-line
+        ></v-select>
+      </v-col>
+    </v-row>
+    
         <div>
           <v-btn @click="vai"> 
             Vai
@@ -22,8 +38,11 @@ let debounceUser = null
 
   export default {
     data: () => ({
+      repo: null,
       user:null,
       userlist: [],
+      repolist: [],
+      repoloading :false,
       userloading: false,
       usersearch: null
     }),
@@ -36,22 +55,32 @@ let debounceUser = null
           this.getUser() 
           debounceUser = null
         }, 760)
+
       },
-      getUser() {
+      async listaRepositorios() {
+          this.repoloading =true 
+          const data = await api.lista_repos(this.user)
+          this.repolist = data
+          this.repoloading = false
+        },
+      async getUser() {
           console.log('perguntei ' + this.usersearch )
           this.userloading = true
-          api.search_users(this.usersearch).then(data => {
+          const data = await api.search_users(this.usersearch)
 
           this.userlist = data.items
           this.userloading = false
-          }
-        )
       }
     },
     watch: {
       usersearch() {
         this.procuraUsersDebounced()
-
+      },
+      user() {
+          this.listaRepositorios(this.user)
+      },
+      repo () {
+          console.log(this.repo)
       }
     }
   }
